28520e9fe181868b0ab22e6dd48dbb72b22dea37
Max-Meldrum <Max-Meldrum@users.noreply.github.com>
2021-08-05 06:32:23 +0000

    deploy: 02e0df73e76fd64f4bbfe96563529c4853285e09

---
+++ 17007de1-9b45b9cebe06ae62ceb3.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkarcon_website=self.webpackChunkarcon_website||[]).push([[876],{25538:function(e,t,n){n.d(t,{L2V:function(){return r}});var l=n(51046);function r(e){return(0,l.w_)({tag:"svg",attr:{viewBox:"0 0 16 16",fill:"currentColor"},child:[{tag:"path",attr:{fillRule:"evenodd",clipRule:"evenodd",d:"M8.024 5.928l-4.357 4.357-.62-.618L7.716 5h.618L13 9.667l-.619.618-4.357-4.357z"}}]})(e)}}}]);
+//# sourceMappingURL=17007de1-9b45b9cebe06ae62ceb3.js.map
